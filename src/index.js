f611888ad22b63c5d1480880dd65d3ee4d1a3948
thenefenyak <nefenyak92@yandex.ru>
2024-06-16 13:30:55 +0300

    add functions

--- src/index.js
+++ src/index.js
@@ -1,32 +1,62 @@
 document
   .querySelector('.player-chunk-prev')
   .addEventListener('click', function () {
-    function moveClass(className, activeClassName) {
-      let active = document.querySelector('.' + activeClassName),
-        prev = active.previousElementSibling
+    moveClass('player-chunk-active', 'previousElementSibling')
+    moveClass('timeline-chunk-active', 'previousElementSibling', (el) => {
+      const inner = el.querySelector('.timeline-chunk-inner'),
+        w = parseFloat(inner.style.width) || 0
 
-      if (prev && prev.classList.contains(className)) {
-        active.classList.remove(activeClassName)
-        prev.classList.add(activeClassName)
-      }
-    }
-
-    moveClass('timeline-chunk', 'timeline-chunk-active')
-    moveClass('player-chunk', 'player-chunk-active')
+      el.querySelector('.timeline-chunk-inner').style.width = ''
+      return w <= 20
+    })
   })
 
-document
-  .querySelector('.player-chunk-next')
-  .addEventListener('click', function () {
-    function moveClass(className) {
-      let active = document.querySelector('.' + className)
+document.querySelector('.player-chunk-next').addEventListener('click', next)
 
-      if (active.nextElementSibling) {
-        active.classList.remove(className)
-        active.nextElementSibling.classList.add(className)
-      }
+function moveClass(className, method, pred) {
+  const active = document.querySelector('.' + className),
+    next = active[method]
+
+  if (pred && !pred(active)) {
+    return null
+  }
+
+  if (next) {
+    active.classList.remove(className)
+    next.classList.add(className)
+
+    return active
+  }
+  return null
+}
+
+function next() {
+  moveClass('player-chunk-active', 'nextElementSibling')
+
+  const el = moveClass('timeline-chunk-active', 'nextElementSibling')
+  if (el) {
+    el.querySelector('.timeline-chunk-inner').style.width = ''
+  }
+}
+let timer
+
+function runInterval(time, step) {
+  clearInterval(timer)
+
+  timer = setInterval(() => {
+    const active = document
+      .querySelector('.timeline-chunk-active')
+      .querySelector('.timeline-chunk-inner')
+
+    const w = parseFloat(active.style.width) || 0
+
+    if (w === 100) {
+      next()
+      return
     }
 
-    moveClass('timeline-chunk-active')
-    moveClass('player-chunk-active')
-  })
+    active.style.width = String(w + step) + '%'
+  }, (time * 1000 * step) / 100)
+}
+
+runInterval(5, 1)
